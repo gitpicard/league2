76f6ce5818e768f9e8e1bd73a3f5020d893d642b
Philip Picard <emailmeforfreeipad@gmail.com>
2020-05-03 10:36:21 -0400

    Forgot to implement abstract methods...

--- sandbox.py
+++ sandbox.py
@@ -5,6 +5,12 @@ GAME_SIZE = (426, 240)
 
 
 class SandboxGame(league2.Application):
+    def on_start(self):
+        pass
+
+    def on_end(self):
+        pass
+
     def on_draw(self, frame_time: float):
         pass
 
